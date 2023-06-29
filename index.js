033b09bf6d904ba7e0dbd724634c1cda1c872f64
Katarzyna Łada <96846727+KasiaLada@users.noreply.github.com>
2023-06-29 21:16:25 +0200

    corrected

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
-const btn = document.querySelector('#calculate');
 const currency = document.querySelector('#currency');
 const input = document.querySelector('#input');
+const form = document.getElementById('form');
 const url = 'https://api.nbp.pl/api/exchangerates/rates/a/';
 const calculateResult = document.querySelector('#calculateResult');
 
-const getCurrencyList = () => {
+const getCurrencyList = (event) => {
+	event.preventDefault();
 	if (input.value && Number(input.value) > 0) {
 		fetch(`${url}/${currency.value}`)
 			.then((response) => response.json())
 			.then((data) => {
-				const mid = data.rates[0].mid;
+				const mid = data?.rates[0]?.mid;
+
+				if (!mid) {
+					window.alert('Błąd pobierania danych z NBP');
+					return;
+				}
+
 				const result = Number(input.value) * mid;
 				calculateResult.innerHTML = result.toFixed(2);
 			})
-			.catch((err) => console.log(err));
-	} else alert('Podaj kwotę');
+			.catch((err) => window.alert(err));
+	} else {
+		alert('Podaj kwotę');
+	}
 };
 
-btn.addEventListener('click', getCurrencyList);
+form.addEventListener('submit', getCurrencyList);
